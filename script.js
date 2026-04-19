cfdc0977a787cf9efe90c104ca4785d3b8a028c8
ceciliam1-design <ccricerocks1@gmail.com>
2026-04-19 13:34:26 -0400

    Fix toggleMode function formatting

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ menuItems.forEach(function(item) {
   const li = document.createElement('li');
   li.textContent = item;
   container.appendChild(li); 
+}); 
 
 const button = document.getElementById("show-special");
 const display = document.getElementById("daily-special");
@@ -16,4 +17,3 @@ button.addEventListener("click", () => {
 
 function toggleMode() {
   document.body.classList.toggle("dark-mode");
-}
